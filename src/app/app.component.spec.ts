25fac3a82d8556029b198477335fbd6a2e2bf98d
Beerendra M C <beerendramc@gmail.com>
2020-12-12 19:43:15 +0530

    ng update @angular/core @angular/cli => 11 (latest)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,20 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    const employeeService = jasmine.createSpyObj('EmployeeService', ['getEmployees']);
+  beforeEach(
+    waitForAsync(() => {
+      const employeeService = jasmine.createSpyObj('EmployeeService', ['getEmployees']);
 
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      providers: [{ provide: EmployeeService, useValue: employeeService }],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
-  }));
+      TestBed.configureTestingModule({
+        declarations: [AppComponent],
+        providers: [{ provide: EmployeeService, useValue: employeeService }],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/custom-data-grid/custom-data-grid.component.spec.ts
+++ src/app/custom-data-grid/custom-data-grid.component.spec.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 
@@ -10,21 +10,23 @@ describe('CustomDataGridComponent', () => {
   let component: CustomDataGridComponent;
   let fixture: ComponentFixture<CustomDataGridComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [MatTableModule, MatSortModule],
-      declarations: [CustomDataGridComponent],
-      providers: [
-        {
-          provide: DatePipe,
-          useValue: {
-            datePipe: jasmine.createSpy('datePipe')
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [MatTableModule, MatSortModule],
+        declarations: [CustomDataGridComponent],
+        providers: [
+          {
+            provide: DatePipe,
+            useValue: {
+              datePipe: jasmine.createSpy('datePipe')
+            }
           }
-        }
-      ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
-  }));
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CustomDataGridComponent);
