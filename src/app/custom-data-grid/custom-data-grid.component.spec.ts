4076b83179c28d594ad042f48dc99575240c2022
Beerendra M C <beerendramc@gmail.com>
2020-12-13 00:25:00 +0530

    unit test case to test grid data

--- src/app/custom-data-grid/custom-data-grid.component.spec.ts
+++ src/app/custom-data-grid/custom-data-grid.component.spec.ts
@@ -1,17 +1,42 @@
 import { DatePipe } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatSortModule, MatTableModule } from '@angular/material';
+import {
+  MatPaginatorModule,
+  MatSelectModule,
+  MatSortModule,
+  MatTableModule,
+  MatTooltipModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColumnType, GridConfig } from '../models/custom-data-grid';
 
 import { CustomDataGridComponent } from './custom-data-grid.component';
 
 describe('CustomDataGridComponent', () => {
   let component: CustomDataGridComponent;
   let fixture: ComponentFixture<CustomDataGridComponent>;
+  const mockGridConfiguration: GridConfig[] = [
+    { name: 'id', label: 'Id', columnType: ColumnType.Text },
+    { name: 'name', label: 'Name', columnType: ColumnType.Link },
+    { name: 'gender', label: 'Gender', columnType: ColumnType.Text }
+  ];
+  const mockDisplayedColumns = ['id', 'name', 'gender'];
+  const mockGridData = [
+    { id: 1, name: 'Beerendra M C', gender: 'male' },
+    { id: 2, name: 'Manju D R', gender: 'male' }
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatTableModule, MatSortModule],
+      imports: [
+        MatTableModule,
+        MatSortModule,
+        MatTooltipModule,
+        MatSelectModule,
+        MatPaginatorModule,
+        BrowserAnimationsModule
+      ],
       declarations: [CustomDataGridComponent],
       providers: [
         {
@@ -33,5 +58,37 @@ describe('CustomDataGridComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should test the table', done => {
+    component.gridConfig = mockGridConfiguration;
+    component.displayedColumns = mockDisplayedColumns;
+    component.dataSource = mockGridData;
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      const tableRows = fixture.nativeElement.querySelectorAll('tr');
+      expect(tableRows.length).toBe(mockGridData.length + 1);
+
+      // Header row
+      const headerRow = tableRows[0];
+      expect(headerRow.cells[0].innerHTML).toContain(mockGridConfiguration[0].label);
+      expect(headerRow.cells[1].innerHTML).toContain(mockGridConfiguration[1].label);
+      expect(headerRow.cells[2].innerHTML).toContain(mockGridConfiguration[2].label);
+
+      // Data rows
+      const row1 = tableRows[1];
+      expect(row1.cells[0].innerHTML).toContain(mockGridData[0].id);
+      expect(row1.cells[1].innerHTML).toContain(mockGridData[0].name);
+      expect(row1.cells[2].innerHTML).toContain(mockGridData[0].gender);
+
+      const row2 = tableRows[2];
+      expect(row2.cells[0].innerHTML).toContain(mockGridData[1].id);
+      expect(row2.cells[1].innerHTML).toContain(mockGridData[1].name);
+      expect(row2.cells[2].innerHTML).toContain(mockGridData[1].gender);
+
+      done();
+    });
   });
 });
